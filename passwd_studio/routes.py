30faa9b86f267a1cb5accf84ead7264512d7db47
rchapman83 <rchapman83@gmail.com>
2019-07-23 07:39:55 +0000

    add test to api

--- passwd_studio/routes.py
+++ passwd_studio/routes.py
@@ -9,7 +9,3 @@ from . import passwd
 from flask import Flask
 from flask_restful import Resource, Api 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-\api.add_resource(HelloWorld, '/')

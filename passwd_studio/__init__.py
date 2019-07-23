5b13455e6b01e7cf2fa541d640760e01bd54d7b1
rchapman83 <rchapman83@gmail.com>
2019-07-23 08:34:44 +0000

    updates made to api

--- passwd_studio/__init__.py
+++ passwd_studio/__init__.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 # Entry point to bring up the web app
 
-# Flask mod imports
-from flask import Flask
-from flask_restful import Resource, Api
-
-# Construct application
-application = Flask(__name__.split('.')[0])
-application.config.from_envvar('FLASK_SETTINGS')
-api = Api(application)
-
-# Import all the views for this web app
+# Import all the views for app
 from . import routes
--- passwd_studio/routes.py
+++ passwd_studio/routes.py
@@ -9,6 +9,11 @@ from . import generator
 from flask import Flask
 from flask_restful import Resource, Api
 
+# Construct application
+application = Flask(__name__.split('.')[0])
+application.config.from_envvar('FLASK_SETTINGS')
+api = Api(application)
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}

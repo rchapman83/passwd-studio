666b2d630c7700e7597fdc9eccf09a3d17cd8bcf
rchapman83 <rchapman83@gmail.com>
2019-07-23 14:00:43 +0000

    update logger

--- run.py
+++ run.py
@@ -14,29 +14,29 @@ a = os.environ.get('APP_MODULE')
 # Timber api token
 t = os.environ.get('TIMBER_TOKEN')
 
-logger = logging.getLogger()
-log_format = '%(asctime)s - %(message)s'
-logging.basicConfig(level=logging.INFO, format=log_format, handlers=[logging.StreamHandler(sys.stdout)])
-timber_handler = timber.TimberHandler(api_key=t, level=logging.INFO, buffer_capacity=20, flush_interval=60, raise_exceptions=True)
-logger.addHandler(timber_handler)
+#log_format = '%(asctime)s - %(message)s'
+#logging.basicConfig(level=logging.INFO, format=log_format)
+#logger = logging.getLogger()
+#timber_handler = timber.TimberHandler(api_key=t, level=logging.INFO, buffer_capacity=20, flush_interval=60, raise_exceptions=True)
+#logger.addHandler(timber_handler)
 
 # Run either Gunicorn or Flask to serve the app
 if x=='0':
     print('Starting application server')
     try:
         subprocess.call(['gunicorn', '-c', c, a])
-        logger.debug('gunicorn start-up complete')
+        #logger.debug('gunicorn start-up complete')
     except RuntimeError as e:
         print('Failed to start-up application server, exiting')
-        logger.error('Failed to start-up gunicorn, exiting. Consider putting application into debug mode' + e)
+        #logger.error('Failed to start-up gunicorn, exiting. Consider putting application into debug mode' + e)
         quit()
 elif x=='1':
     print('Starting application in debug mode')
-    logger.debug('Starting flask app server in debug mode')
+    #logger.debug('Starting flask app server in debug mode')
     from flask import Flask
     from passwd_studio import application
     application.run(host='0.0.0.0', port=8080, debug=True)
 else:
     print('Invalid start-up configuration')
-    logger.critical('Invalid start-up configuration for run.py, quitting')
+    #logger.critical('Invalid start-up configuration for run.py, quitting')
     quit()

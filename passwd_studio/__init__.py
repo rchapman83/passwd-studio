6c91277b55c1f4698cdafca1562b7ae8f67bbb7e
rchapman83 <rchapman83@gmail.com>
2019-07-24 02:22:52 +0000

    update logging

--- passwd_studio/__init__.py
+++ passwd_studio/__init__.py
@@ -3,10 +3,9 @@
 # Import Flask stuff
 from flask import Flask
 from flask_restful import Resource, Api
-import logging
+
 # Construct application
 application = Flask(__name__.split('.')[0])
 application.config.from_envvar('FLASK_SETTINGS')
-logger_timber.info('load flask app')
 # Import all the views for app
 from . import routes
--- passwd_studio/settings.py
+++ passwd_studio/settings.py
@@ -14,23 +14,10 @@ TESTING = False
 USE_X_SENDFILE = False
 CSRF_ENABLED = True
 SECRET_KEY = secret_code
-LOGGING = {
-    'version': 1,
-    'handlers': {
-        'timber': {
-            'level': 'INFO',
-            'class': 'timber.TimberHandler',
-            'api_key': timber_key,
-            'source_id': timber_id, 
-            'buffer_capacity': 20, 
-            'flush_interval': 60, 
-            'raise_exceptions': True
-        },
-    },
-    'loggers': {
-        'logger_timber': {
-            'handlers': ['timber'],
-            'level': 'INFO',
-        }
-    },
-}
+
+# LOGGING
+LOGGER_NAME = 'logger_timber'
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+#timber_handler = timber.TimberHandler(source_id=timber_id, api_key=timber_key, level=logging.INFO, buffer_capacity=20, flush_interval=60, raise_exceptions=True)
+logger_timber.addHandler(timber_handler)

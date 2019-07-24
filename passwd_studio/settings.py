0b290543c9be1c653607600f9073463f1b9c3770
rchapman83 <rchapman83@gmail.com>
2019-07-24 03:16:48 +0000

    update to settings

--- passwd_studio/settings.py
+++ passwd_studio/settings.py
@@ -9,17 +9,32 @@ timber_id = os.environ.get('TIMBER_ID')
 proj_name = os.environ.get('PROJECT_NAME')
 secret_code = os.environ.get('FLASK_SECRET')
 
+# Base config
 DEBUG = False
 TESTING = False
 USE_X_SENDFILE = False
 CSRF_ENABLED = True
+APPLICATION_ROOT = '/'
+SERVER_NAME = None
+#SERVER_NAME = 'api.passwd.studio:8080'
 SECRET_KEY = secret_code
+PREFERRED_URL_SCHEME = 'http'
+
+# Cookie settings
+SESSION_COOKIE_NAME = 'session'
+SESSION_COOKIE_DOMAIN = None
+SESSION_COOKIE_PATH = None
+SESSION_COOKIE_HTTPONLY = True
+
+# JSON & Jsonify 
+JSON_AS_ASCII = True
+JSON_SORT_KEYS = True
 
 # LOGGING
 LOG_LEVEL = logging.INFO
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 logger_timber = logging.getLogger(__name__)
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
-#timber_handler = timber.TimberHandler(source_id=timber_id, api_key=timber_key, level=logging.INFO, buffer_capacity=20, flush_interval=60, raise_exceptions=True)
-#logger_timber.addHandler(timber_handler)
+timber_handler = timber.TimberHandler(source_id=timber_id, api_key=timber_key, level=logging.INFO, buffer_capacity=20, flush_interval=60, raise_exceptions=True)
+logger_timber.addHandler(timber_handler)
 LOGGER_NAME = 'logger_timber'
